6120d4676b01668c12f7f21f7714282bdb428e98
Simon Iong <Simon.Iong@laxino.com>
2013-05-30 23:05:08 +0800

    add Vagrantfile & Berksfile

---
+++ Berksfile
@@ -0,0 +1,9 @@
+site :opscode
+
+cookbook 'nginx', '~> 0.101.5'
+cookbook 'mysql'
+cookbook 'git'
+cookbook 'vim'
+cookbook 'rvm', github: 'fnichol/chef-rvm'
+cookbook 'user', github: 'fnichol/chef-user'
+#cookbook 'ruby-shadow', github: 'cookbooks/ruby-shadow'
---
+++ add_source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm 
+
+rm remi-release-6*.rpm
+rm epel-release-6*.rpm
